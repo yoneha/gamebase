9e69277775d9d41eb545551537da4d4e63256522
hayato.yonehara <hayato.yonehara1229@gmail.com>
2020-09-26 08:07:48 +0000

    入力値バリデーション追加

--- app/models/review.rb
+++ app/models/review.rb
@@ -2,8 +2,17 @@ class Review < ApplicationRecord
   belongs_to :user
   belongs_to :game
   
-  validates :score, presence: true
-  validates :playtime, presence: true
+  validates :score, presence: true,
+            :numericality => {
+              :greater_than_or_equal_to => 0,
+              :less_than_or_equal_to => 10,
+              :message => '入力値が0~10の範囲外です' 
+           }
+  validates :playtime, presence: true,
+            :numericality => {
+              :greater_than_or_equal_to => 0,
+              :message => '入力値が正の整数以外です' 
+           }
   validates :title, presence: true, length: { maximum: 50 }
   validates :content, presence: true, length: { maximum: 1000 }
   
